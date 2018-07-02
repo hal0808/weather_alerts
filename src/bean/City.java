9a1eff7ec2c5606a08396559d0ed8476bdd0db15
haruna <haruna@LAPTOP-RA9U9D0Q>
2018-07-02 12:32:22 +0900

    second commit

--- src/bean/City.java
+++ src/bean/City.java
@@ -1,5 +1,48 @@
 package bean;
 
 public class City {
+	private int code;	//コードID
+	private String name;	//都市名
+	private double lat;		//緯度
+	private double lon;		//経度
+
+
+
+	public City(int code, String name, double lat, double lon) {
+		this.code = code;
+		this.name = name;
+		this.lat = lat;
+		this.lon = lon;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLon() {
+		return lon;
+	}
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+
 
 }
+
+
--- src/bean/Weather.java
+++ src/bean/Weather.java
@@ -1,5 +1,14 @@
 package bean;
 
-public class Weather {
+import java.util.Date;
 
+public class Weather {
+	private int id;			//天気ID(テスト)
+	private Date update;	//更新日時
+	private int code;		//都市コード
+	private Date datetime;	//取得日時
+	private double temp_min;	//最低気温
+	private double temp_max;	//最高気温
+	private String icon;		//画像名
+	private String weather;		//天気
 }
