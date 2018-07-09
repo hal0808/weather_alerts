2e771150f30fd8489e30412efa40e31319d02f3c
masa <masa@yuri>
2018-07-09 12:30:52 +0900

    yano4

--- src/method/Create_record.java
+++ src/method/Create_record.java
@@ -1,10 +1,27 @@
 package method;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+/*
+ * @author masa
+ */
 public class Create_record {
 
 	public boolean execute(HttpServletRequest request) {
+		try {
+			Document doc =Jsoup.connect("").get();
+
+
+
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 
 
 
