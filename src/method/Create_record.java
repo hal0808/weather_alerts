553642826d2297c6d05f8f2c19a54bfcb6273e09
masa <masa@yuri>
2018-07-11 10:47:49 +0900

    masa

--- src/method/Create_record.java
+++ src/method/Create_record.java
@@ -7,14 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-/*
+/**
  * @author masa
  */
 public class Create_record {
 
-	public boolean execute(HttpServletRequest request) {
+	public boolean execute(HttpServletRequest request,int city_id) {
+
 		try {
-			Document doc =Jsoup.connect("").get();
+			Document doc =Jsoup.connect("http://api.openweathermap.org/data/2.5/forecast?id=1852561&appid=d3eb516e0fd441b11ce433f19fdbc480").get();
+
 
 
 
