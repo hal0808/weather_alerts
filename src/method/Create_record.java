d7120e3d619ef7cbf68118e6ed88d5cebb345f7c
masa <masa@yuri>
2018-07-13 09:41:51 +0900

    yano

--- src/method/Create_record.java
+++ src/method/Create_record.java
@@ -1,11 +1,6 @@
 package method;
 
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletRequest;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 /**
  * @author masa
@@ -14,16 +9,17 @@ public class Create_record {
 
 	public boolean execute(HttpServletRequest request,int city_id) {
 
-		try {
-			Document doc =Jsoup.connect("http://api.openweathermap.org/data/2.5/forecast?id=1852561&appid=d3eb516e0fd441b11ce433f19fdbc480").get();
 
-
-
-
-		} catch (IOException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+//		try {
+//			Document doc =Jsoup.connect("http://api.openweathermap.org/data/2.5/forecast?id=1852561&appid=d3eb516e0fd441b11ce433f19fdbc480").get();
+//			System.out.println(doc);
+//
+//
+//
+//		} catch (IOException e) {
+//			// TODO 自動生成された catch ブロック
+//			e.printStackTrace();
+//		}
 
 
 
